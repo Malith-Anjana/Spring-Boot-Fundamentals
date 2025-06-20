2c95f2fbbc10d9433d78820fa1c63937e72a9a9f
Malith Anjana <malithanjana97@gmail.com>
2025-06-20 12:12:18 +0530

    Constructor Injection

---
+++ src/main/java/com/learnspring/store/OrderService.java
@@ -0,0 +1,11 @@
+package com.learnspring.store;
+
+public class OrderService {
+    private PaymentService paymentService;
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+    public void placeOrder(){
+        paymentService.processPayment(10);
+    }
+}
